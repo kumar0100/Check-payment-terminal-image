63bfccfa6d59201bedab50186b5181906bb04b6f
Kumar Zhekshenbek uulu <zhekshenbekukumar@gmail.com>
2025-03-07 01:13:19 +0600

    Improved the project

--- src/__tests__/verify-image.test.ts
+++ src/__tests__/verify-image.test.ts
@@ -1,44 +1,40 @@
 /* Local dependencies */
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import verifyImage from "../index"
 
 describe('Verify-image function', () => {
+  test('It should return true if the image is valid', async () => {
+    const terminalImagePath = path.join(__dirname, "test-images", "terminal1.jpeg");
+    const imageBuffer = fs.readFileSync(terminalImagePath);
+    
+    const result = await verifyImage(imageBuffer);
+    
+    expect(result).toBe(true);
+  }, 15000);
+
+  test('It should return true if the image is valid', async () => {
+    const terminalImagePath = path.join(__dirname, "test-images", "terminal.jpg");
+    const imageBuffer = fs.readFileSync(terminalImagePath);
+    
+    const result = await verifyImage(imageBuffer);
+    
+    expect(result).toBe(true);
+  }, 15000);
+
   test("It should return false if the image is unvalid", async () => {
-    const nonTerminalImage = path.join(
-      __dirname,
-      "../test-images/non-terminal.jpg"
-    );
+    const nonTerminalImage = path.join(__dirname, "test-images", "non-terminal.jpg");
     const imageBuffer = fs.readFileSync(nonTerminalImage);
+
     const result = await verifyImage(imageBuffer);
 
     expect(result).toBe(false);
   }, 15000);
 
-  test('It should return true if the image is valid', async () => {
-    const terminalImage = path.join(__dirname, "../test-images/terminal1.jpeg");
-    const imageBuffer = fs.readFileSync(terminalImage)
-
-    const result = await verifyImage(imageBuffer);
-
-    expect(result).toBe(true)
-  }, 15000)
-
-  test("It should return true if the image is valid", async () => {
-    const terminalImage = path.join(__dirname, "../test-images/terminal.jpg");
-    const imageBuffer = fs.readFileSync(terminalImage);
-
-    const result = await verifyImage(imageBuffer);
-
-    expect(result).toBe(true);
-  }, 15000);
-
-  test("It should return false if the image is unvalid", async () => {
-    const nonTerminalImage = path.join(
-      __dirname,
-      "../test-images/non-terminal1.jpg"
-    );
+  it("Should return false if the image is unvalid", async () => {
+    const nonTerminalImage = path.join(__dirname, "test-images", "non-terminal1.jpg");
     const imageBuffer = fs.readFileSync(nonTerminalImage);
+
     const result = await verifyImage(imageBuffer);
 
     expect(result).toBe(false);
